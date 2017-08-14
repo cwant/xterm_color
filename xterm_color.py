fce195e3da53e6b767507f0b87113b8df678dd41
Chris Want <cjwant@gmail.com>
2017-08-14 16:07:55 -0600

    Add cyan and magenta terminals.

--- xterm_color.py
+++ xterm_color.py
@@ -5,7 +5,8 @@ from random import randint
 
 def main():
   parser = optparse.OptionParser(description='Dark colors for xterms.')
-  parser.add_option('--favour', choices=["red", "green", "blue", "grey"])
+  parser.add_option('--favour', choices=["red", "green", "blue",
+                                         "cyan", "magenta", "grey"])
 
   (options, args) = parser.parse_args()
 
@@ -18,6 +19,8 @@ def main():
   if (options.favour == "grey"):
     i = randint(20, 60)
     c = [i, i, i]
+  elif (options.favour == "magenta" or options.favour == "cyan"):
+    c = get_averaged_color(min_c1, max_c1)
   else:
     c = get_color(min_c1, max_c1)
 
@@ -27,11 +30,16 @@ def main():
     print "rgb:%02x/%02x/%02x" % (c[1],c[0],c[2])
   elif (options.favour == "blue"):
     print "rgb:%02x/%02x/%02x" % (c[1],c[2],c[0])
+  elif (options.favour == "magenta"):
+    print "rgb:%02x/%02x/%02x" % (c[0],c[2],c[1])
+  elif (options.favour == "cyan"):
+    print "rgb:%02x/%02x/%02x" % (c[2],c[0],c[1])
   else:
     o = rand_order()
     print "rgb:%02x/%02x/%02x" % (c[o[0]],c[o[1]],c[o[2]])
 
 def get_color(mn, mx):
+  # c1 is highest, then c2, then c3
   c1 = randint(mn, mx)
   mx = mx - c1
 
@@ -39,6 +47,12 @@ def get_color(mn, mx):
   c3 = mx - c2
 
   return (c1, c2, c3)
+
+def get_averaged_color(mn, mx):
+  # Average the highest two coords (the first two)
+  c = get_color(mn, mx)
+  ave = (c[0] + c[1]) / 2
+  return (ave, ave, c[2])
 
 def rand_order():
   orders = ( (0, 1, 2),
